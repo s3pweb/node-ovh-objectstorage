836d036f984f4463f01a1a6c8b9b82beac691ca1
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2018-09-24 10:22:57 +0200

    test(authent): switch Object.keys to hasOwnProperty

--- test/authent.test.js
+++ test/authent.test.js
@@ -10,7 +10,17 @@ test('Login to OVH', async t => {
 
   let values = await storage.connection()
 
-  t.truthy(values, 'Values should exist')
-  t.deepEqual(Object.keys(values.token), ['issued_at', 'expires', 'id', 'tenant', 'audit_ids'], 'Token should contains all the keys')
-  t.deepEqual(Object.keys(values.endpoint), ['adminURL', 'region', 'id', 'internalURL', 'publicURL'], 'Endpoint should contains all the keys')
+  t.truthy(values, 'Login response should have a body.')
+
+  t.true(values.token.hasOwnProperty('audit_ids'), 'Token should have audit_ids property.')
+  t.true(values.token.hasOwnProperty('expires'), 'Token should have expires property.')
+  t.true(values.token.hasOwnProperty('id'), 'Token should have id property.')
+  t.true(values.token.hasOwnProperty('issued_at'), 'Token should have issued_at property.')
+  t.true(values.token.hasOwnProperty('tenant'), 'Token should have tenant property.')
+
+  t.true(values.endpoint.hasOwnProperty('adminURL'), 'Endpoint should have adminURL property.')
+  t.true(values.endpoint.hasOwnProperty('id'), 'Endpoint should have id property.')
+  t.true(values.endpoint.hasOwnProperty('internalURL'), 'Endpoint should have internalURL property.')
+  t.true(values.endpoint.hasOwnProperty('publicURL'), 'Endpoint should have publicURL property.')
+  t.true(values.endpoint.hasOwnProperty('region'), 'Endpoint should have region property.')
 })
